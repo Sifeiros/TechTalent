dc6a5cab1c47b813ed637335efe949e5dfba6b3f
Thomas Maqua <thomas.maqua@senacor.com>
2016-12-15 18:22:39 +0100

    State!

--- frontend/src/components/PersonSearchForm/PersonSearchForm.js
+++ frontend/src/components/PersonSearchForm/PersonSearchForm.js
@@ -1,45 +1,43 @@
 import React from 'react';
-import { Typeahead } from 'react-bootstrap-typeahead';
+import {Typeahead} from 'react-bootstrap-typeahead';
 
 class PersonSearchForm extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
-    this.state = {
-      skills: '',
-      infer: false
-    };
+    this.state = this.props.params;
 
     this.updateSkills = this.updateSkills.bind(this);
     this.updateInfer = this.updateInfer.bind(this);
   }
 
-  componentDidUpdate (prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (JSON.stringify(prevState) !== JSON.stringify(this.state)) {
       this.props.setSearchParams(this.state);
     }
   }
 
-  updateSkills (skills) {
-    this.setState({ skills });
+  updateSkills(skills) {
+    this.setState({skills});
   }
 
-  updateInfer (event) {
-    this.setState({ infer: !!event.target.checked });
+  updateInfer(event) {
+    this.setState({infer: !!event.target.checked});
   }
 
-  render () {
+  render() {
     return (
       <form>
         <div className='form-group'>
           <Typeahead onChange={this.updateSkills}
-            options={this.props.options}
-            placeholder='Skills'
-            multiple />
+                     options={this.props.options}
+                     placeholder='Skills'
+                     defaultSelected={this.state.skills}
+                     multiple/>
         </div>
         <div className='checkbox'>
           <label>
-            <input type='checkbox' onChange={this.updateInfer} /> Allow inferred
+            <input type='checkbox' onChange={this.updateInfer} checked={this.state.infer} /> Allow inferred
           </label>
         </div>
       </form>
@@ -49,7 +47,7 @@ class PersonSearchForm extends React.Component {
 
 PersonSearchForm.propTypes = {
   setSearchParams: React.PropTypes.func.isRequired,
-  options: React.PropTypes.object.isRequired
+  options: React.PropTypes.array.isRequired
 };
 
 export default PersonSearchForm;
