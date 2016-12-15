e40c469213c26d93e31c496c5483f28caa6784ef
Alasdair Collinson <Alasdair.Collinson@Senacor.com>
2016-12-15 11:17:14 +0100

    Switched to express, added route for persons (with mockdata)

--- servicelayer/src/server.js
+++ servicelayer/src/server.js
@@ -1,24 +1,35 @@
-var http = require('http');
-var logger = require('winston');
+var express = require('express');
+var app = express();
+var winston = require('winston');
+var logger = new (winston.Logger) ({
+  transports: [
+    new (winston.transports.Console) ({ level: 'info' }),
+    new (winston.transports.File) ({
+      filename: 'server-debug.log',
+      level: 'debug'
+    })
+  ]
+});
 
-// Pretty much copied from https://blog.nodejitsu.com/a-simple-webservice-in-nodejs/
+var persons = require('./person-mocks');
+
 exports.createServer = function (port) {
-  var server = http.createServer(function(request, response) {
-    var data = '';
-
-    logger.info('Incoming Request', { url: request.url });
-
-    request.on('data', function (chunk) {
-      data += chunk;
-    });
-
-    response.writeHead(501, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify({ message: 'not implemented'}));
+  app.get('/', function(req, res) {
+    res.status(501);
+    res.send(JSON.stringify({ message: 'not implemented'}));
   });
 
-  if (port) {
-    server.listen(port);
-  }
+  app.get('/persons', function(req, res) {
+    res.status(200);
+    res.send(JSON.stringify(persons));
+  });
+
+  var server = app.listen(port, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    logger.debug('Server running at http://%s:%s', host, port);
+  });
 
   return server;
 };
