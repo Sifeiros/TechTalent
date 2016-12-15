d8b3c0219ec1d91af1a334ef3c55a97de280e563
Alasdair Collinson <Alasdair.Collinson@Senacor.com>
2016-12-15 21:33:27 +0100

    Display numbers as actual numbers

--- servicelayer/src/skills-search.js
+++ servicelayer/src/skills-search.js
@@ -18,8 +18,23 @@ function getListOfSkills(dbresult) {
 exports.skillsWithStatistics = function (callback) {
   var query = cypher()
     .match("(p:Person)-[edge:KNOWS]->(s:Skill)")
-    .with("s AS s, { id: p.id, values: {level: edge.level, affinity: edge.affinity} } as connections")
+    .with("s AS s, { id: p.id, level: edge.level, affinity: edge.affinity } as connections")
     .return("s.name AS skill, collect(connections) AS values");
 
-  db.cypher({query: query.compile(true)}, callback);
+  db.cypher({query: query.compile(true)}, function(err, result) { return callback(err, allNumbersAsNumbers(result)); });
 };
+
+function allNumbersAsNumbers(skillset) {
+  return _.map(skillset, function(skillEntry) {
+    return {
+      skill: skillEntry.skill,
+      values: _.map(skillEntry.values, function(value) {
+        return {
+          id: value.id,
+          level: parseInt(value.level),
+          affinity: parseInt(value.affinity)
+        }
+      })
+    }
+  })
+}
